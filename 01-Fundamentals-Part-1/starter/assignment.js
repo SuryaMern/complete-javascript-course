c8f9057dbac5a132b3ec1f0aaedd6d2ecc2a4268
SuryaMERN <krishsurya98@gmail.com>
2023-03-24 18:47:58 +0530

    okie

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,59 @@
+//Variables Example
+/* let country = "india"
+console.log(country);
+console.log(typeof country);
+let continent = "Asia"
+console.log(continent);
+console.log(typeof continent);
+let population = "1.4Million"
+console.log(population);
+console.log(typeof population); */
+
+
+//Datatype Example
+/* let isIsLand = "India"
+console.log(isIsLand);
+isIsLand = true;
+console.log(typeof isIsLand);
+
+let language;
+console.log(typeof language);
+
+//let,const 
+//1.using const
+ const isLanguage = "kannada"
+console.log(isLanguage);
+isLanguage="tamil"
+console.log(isLanguage);  */
+
+//2.using let
+ /* let knownLanguage = "kanada"
+console.log(knownLanguage);
+knownLanguage = "tamil"
+console.log(knownLanguage);
+
+//3.using var
+var localLanguage = "tamil"
+localLanguage = "telugu"
+console.log(localLanguage);  */
+
+/* let pop = 140000000
+let halfPop = pop / 2
+console.log(halfPop);
+halfPop++
+console.log(halfPop);
+popFin = 60000000
+popInd = 140000000
+console.log(popFin < popInd);
+let description = 'Portugal is in Europe, and its 11 million people speak portuguese'
+console.log(description); */
+
+//String literals
+
+let firstName = "surya"
+let job = "teacher"
+let birthYear = 1998
+let year = 2023
+
+let emp_details = `I'm ${firstName} and my profession is a ${job} and my birthyear ${year-birthYear}`
+console.log(emp_details);
