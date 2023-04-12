b36cf7ef4ef71614ba9ae55f1feba1144654af05
SuryaMERN <krishsurya98@gmail.com>
2023-04-12 18:48:33 +0530

    okie

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -31,16 +31,16 @@ the same populations) */
 
 //Function Declaration
 /* const population = 1393.3
-const worldPop=7900 */
+const worldPop=7900  */
 
 // const country = "India"
 /* function percentageOfWorld1(country,population,worldPop){
         return `${country} is a ${population}million people, so it's about ${ (population /worldPop)*100
         } of the world population`
-}
-console.log(percentageOfWorld1("India",1393.3,7900))
+} */
+/* console.log(percentageOfWorld1("India",1393.3,7900))
 console.log(percentageOfWorld1("china",1441,7900))
-console.log(percentageOfWorld1("America",334.9,7900)) */
+console.log(percentageOfWorld1("America",334.9,7900))  */ 
 
 //Function Expression
 
@@ -82,7 +82,7 @@ console.log(describePopulation("china",1441));
 console.log(describePopulation("India",1393.3));
 console.log(describePopulation("America",334.9)); */
 
-LECTURE: Introduction to Arrays
+/* LECTURE: Introduction to Arrays
 1. Create an array containing 4 population values of 4 countries of your choice. 
 You may use the values you have been using previously. Store this array into a 
 variable called 'populations'
@@ -90,4 +90,112 @@ variable called 'populations'
 3. Create an array called 'percentages' containing the percentages of the 
 world population for these 4 population values. Use the function 
 'percentageOfWorld1' that you created earlier to compute the 4 
-percentage values
+percentage values */
+
+/* const population = [1441, 1393, 335, 798];
+// console.log(population.length === 4);
+const worldPop = 7900;
+const country = ["China", "India", "USA", "UK"];
+const percentages = [];
+function percentageOfWorld1() {
+  for (i = 0; i <= population.length - 1 && i <= country.length - 1; i++) {
+    const sum = `${(population[i] / worldPop) * 100}`;
+    console.log(
+      `${country[i]}is a ${population[i]}million people, so it's about ${
+        (population[i] / worldPop) * 100
+      } of the world population`
+    );
+    percentages.push(sum);
+  }
+}
+
+percentageOfWorld1();
+console.log(percentages); */
+
+/* 
+
+LECTURE: Basic Array Operations (Methods)
+1. Create an array containing all the neighbouring countries of a country of your 
+choice. Choose a country which has at least 2 or 3 neighbours. Store the array 
+into a variable called 'neighbours'
+2. At some point, a new country called 'Utopia' is created in the neighbourhood of 
+your selected country. So add it to the end of the 'neighbours' array
+3. Unfortunately, after some time, the new country is dissolved. So remove it from 
+the end of the array
+4. If the 'neighbours' array does not include the country ‘Germany’, log to the 
+console: 'Probably not a central European country :D'
+5. Change the name of one of your neighbouring countries. To do that, find the 
+index of the country in the 'neighbours' array, and then use that index to 
+change the array at that index position. For example, you can search for
+'Sweden' in the array, and then replace it with 'Republic of Sweden' */
+
+/* const neighbours = ["china","srilanka","Nepal"]
+neighbours.push("Utopia")
+console.log(neighbours);
+neighbours.pop()
+console.log(neighbours);
+if(!neighbours.includes("Germany")){
+    console.log('Probably not a central European country :D');
+}
+neighbours[neighbours.indexOf("china")] ="Republic of china"
+console.log(neighbours); */
+
+/* 
+LECTURE: Introduction to Objects
+1. Create an object called 'myCountry' for a country of your choice, containing 
+properties 'country', 'capital', 'language', 'population' and 
+'neighbours' (an array like we used in previous assignments) */
+
+/* const myCountry = {
+    country:"India",
+    capital:"New Delhi",
+    language:"Hindi",
+    Population:1393,
+    neighbours:["China","Nepal","Srilanka"]
+}
+console.log(myCountry); */
+
+/* LECTURE: Dot vs. Bracket Notation
+1. Using the object from the previous assignment, log a string like this to the 
+console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries 
+and a capital called Helsinki.'
+2. Increase the country's population by two million using dot notation, and then 
+decrease it by two million using brackets notation */
+
+/* const myCountry = {
+    country:"India",
+    capital:"New Delhi",
+    language:"Hindi",
+    Population:1393,
+    neighbours:["China","Nepal","Srilanka"]
+}
+console.log(`${myCountry.country} has ${myCountry.Population} ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`);
+
+myCountry.Population += 2
+console.log(myCountry);
+myCountry["Population"] -= 2
+console.log(myCountry); */
+
+/* LECTURE: Iteration: The for Loop
+1. There are elections in your country! In a small town, there are only 50 voters. 
+Use a for loop to simulate the 50 people voting, by logging a string like this to 
+the console (for numbers 1 to 50): 'Voter number 1 is currently voting' */
+
+/* for(let voter=1;voter<=50;voter++){
+    console.log(voter,`voter number ${voter} is currently voting`);
+} */
+
+
+/* LECTURE: Looping Arrays, Breaking and Continuing
+1. Let's bring back the 'populations' array from a previous assignment
+2. Use a for loop to compute an array called 'percentages2' containing the 
+percentages of the world population for the 4 population values. Use the 
+function 'percentageOfWorld1' that you created earlier
+3. Confirm that 'percentages2' contains exactly the same values as the 
+'percentages' array that we created manually in the previous assignment, 
+and reflect on how much better this solution is */
+
+const population = [1441, 1393, 335, 798];
+for(population=0;population<4;population++){
+    console.log(object);
+}
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,26 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/* const x = '10';
+console.log(x);
+console.log(x);
+
+if (0 == '0') {
+  console.log('hi');
+}
+
+const calcAge = birthYear => 2023 - birthYear;
+console.log(calcAge(1998));
+
+if (true == 1) {
+  console.log('hi');
+} */
+
+const calcTempAmplified = function (temp) {
+  let max = temp[0];
+  for (let i = 0; i < temp.length; i++) {
+    if (temp[i] > max) max = temp[i];
+  }
+  return max;
+};
+console.log(calcTempAmplified(1, 3, 4));
