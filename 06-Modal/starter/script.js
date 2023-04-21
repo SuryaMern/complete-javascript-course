fb46171b56b1d334e1be5ee8412673c8e5e40a9f
SuryaMERN <krishsurya98@gmail.com>
2023-04-21 15:19:24 +0530

    okie

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,10 +3,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
 const openModal = function () {
-  console.log('Button Clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -16,17 +14,16 @@ const closeModal = function () {
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].addEventListener('click', openModal));
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  console.log(e.key);
-});
-if (e.key === 'Escape') {
-  if (!modal.classList.contains('hidden')) {
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
-}
+});
