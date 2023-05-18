1d926a3c53a6d8739340189a9f2e5ab5b5ad092f
SuryaMERN <krishsurya98@gmail.com>
2023-05-18 19:04:10 +0530

    okie

---
+++ 11-Arrays-Bankist/starter/practice.js
@@ -0,0 +1,45 @@
+//Array Methods
+//Slice Method
+
+// const add = [1, 2, 3, 4, 5];
+// console.log(add.slice(0));
+// console.log(add.slice(2, 4));
+// console.log(add);
+// console.log(add.slice(-1));
+// console.log(add.slice(1, -2));
+
+//Splice Method
+// const wish = [8, 2, 4, 1, 5];
+// console.log(wish.splice(0));
+// console.log(wish.splice(1, 3));
+// console.log(wish);
+
+//Concat Method
+/* const text = ['one', 'two', 'three', 'four', 'five'];
+console.log(text.concat()); */
+
+//Join Method
+/* const add = [1, 2, 3, 4, 5];
+const wish = [6, 7, 8, 9, 10];
+console.log(add.concat(wish)); */
+
+//FOR EACH METHOD
+
+/* const add = [132, -456, 435, 673, -673, 5564, -335, 678, -942];
+
+for (a of add) {
+  if (a > 0) {
+    console.log(`you deposited ${a}`);
+  } else {
+    console.log(`you withdraw ${Math.abs(a)}`);
+  }
+}
+
+console.log('-----forEach------');
+add.forEach(function (name) {
+  if (name > 0) {
+    console.log(`you deposited ${name}`);
+  } else {
+    console.log(`you withdraw ${Math.abs(name)}`);
+  }
+}); */
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -5,7 +5,7 @@
 // BANKIST APP
 
 // Data
-const account1 = {
+/* const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
@@ -71,6 +71,119 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]; */
 
 /////////////////////////////////////////////////
+
+//ARRAY METHODS
+//1.SLICE Methods
+
+// const add = ['a', 'b', 'c', 'd', 'e'];
+/* console.log(add.slice(2));
+console.log(add.slice(2, 4));
+console.log(add.slice(-2));
+console.log(add.slice(-1));
+console.log(add.slice(1, -1)); */
+
+//2.SPLICE METHOD
+// const add = ["f","g","h","i","j"]
+// console.log(add.splice(1, 3));
+// console.log(add.splice(2));
+// console.log(add);
+
+// 3.REVERSE METHOD
+//it doesn't mutate
+/* const wish = [5, 4, 3, 2, 1];
+console.log(wish.reverse());
+console.log(wish); */
+
+// 4.CONCAT METHOD
+
+/* const a = ['a', 'b', 'c', 'd', 'e', 'f'];
+const b = ['g', 'h', 'i', 'j', 'k', 'l'];
+
+console.log(a.concat(b));
+console.log([...a, ...b]); //Spread operator */
+
+//5.JOIN METHOD
+/* const add = [1, 2, 3, 4, 5];
+const wish = [6, 7, 8, 9, 10];
+console.log(add.concat(wish)); */
+
+// 6.AT METHOD
+/* const add = [1, 2, 45, 742, 225, 521, 456];
+console.log(add.at(0));
+console.log(add[0]); //we can use this one or above one both will give same result
+console.log(add[add.length - 1]);
+console.log(add.at(-1)); */
+
+//FOR EACH METHOD IN ARRAY
+//first one current element of array
+//second one is index value of array
+//third one is total array
+/* const movements = [423, -134, 248, -284, 954, 353, -345, 483, -356];
+
+for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You Deposited ${movement}`);
+  } else {
+    console.log(`You withdraw ${Math.abs(movement)}`);
+  }
+}
+
+console.log('---ForEach Method---');
+movements.forEach(function (move) {
+  if (move > 0) {
+    console.log(`You Deposited ${move}`);
+  } else {
+    console.log(`You withdraw ${Math.abs(move)}`);
+  }
+}); */
+
+//0:function(423)
+//1:function(134)
+//2:function(248)
+
+/* const statements = [
+  245, -567, 2536, -356, 858, -596, -229, 602, 938, -682, 562, -285, 592, -359,
+  483, -693,
+];
+
+for (const [state, statement] of statements.entries()) {
+  if (statement > 0) {
+    console.log(`Movement ${state + 1}: You Deposited ${statement}`);
+  } else {
+    console.log(`Movement ${state + 1}:  you withdraw ${Math.abs(statement)}`);
+  }
+}
+
+console.log('------forEach-------');
+
+statements.forEach(function (state, i, arr) {
+  if (state > 0) {
+    console.log(`Movement ${i + 1}: you deposited ${state}`);
+  } else {
+    console.log(`Movement ${i + 1}: you withdraw ${Math.abs(state)}`);
+  }
+}); */
+
+//FOR EACH METHOD WITH MAPS AND SETS
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+/* currencies.forEach(function (value, key, arr) {
+  console.log(`${key}: ${value}`);
+}); */
+
+//SETS IN FOREACH
+
+/* const order = new Set(['USD', 'GBR', 'USD', 'ESR', 'RCB', 'GBR']);
+console.log(order);
+
+order.forEach(function (value, _, arr) {
+  console.log(`${value}: ${value}`);
+}); */
